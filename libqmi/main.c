0b0b62dc402cd1f7b059bf6a843ac5eda5a85e40
Joshua Hill <posixninja@gmail.com>
2013-07-08 13:06:57 -0400

    Initial Commit

---
+++ libqmi/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  libqmi
+//
+//  Created by Joshua Hill on 7/8/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+	return 0;
+}
+
